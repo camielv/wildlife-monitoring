6bb882385ee787188bdb7afbe9d8be241ef0326e
Camiel Verschoor <verschoor@uva.nl>
2014-05-08 17:02:29 +0200

    moved all kinds of stuff

--- src/modules/datastructures/detection.py
+++ src/modules/datastructures/detection.py
@@ -1,5 +1,5 @@
-class Detection():
-  '''Class representing an detection'''
+class TrackDetection():
+  '''Class representing an tracked detection'''
   def __init__(self, id, frame_id, bounding_box, real_id):
     self.alive = True
     self.id = id
@@ -14,4 +14,17 @@ class Detection():
     return_string += "Alive: %d\n" % (int(self.alive))
     return_string += "Frames:%s\n" % ("".join(" " + str(frame_id) for frame_id in self.frames))
     return_string += "Bounding box: %s" % str(self.bounding_box)
+    return return_string
+
+class Detection():
+  '''Class representing an detection'''
+  def __init__(self, frame_id, (xmin, ymin, xmax, ymax), confidence):
+    self.frame_id = int(frame_id)
+    self.bounding_box = (int(xmin), int(ymin), int(xmax), int(ymax))
+    self.confidence = float(confidence)
+    
+  def __str__(self):
+    return_string  = "Frame: %d\n" % self.frame_id
+    return_string += "Bounding box: %s\n" % self.bounding_box
+    return_string += "Confidence: %f" % self.confidence
     return return_string
